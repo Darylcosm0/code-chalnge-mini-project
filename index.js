84e0c570e257f9dacb007a66fc1274630517f241
Darylcosm0 <mutaidaryl@gmail.com>
2022-12-04 21:34:57 +0300

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const submitButton = document.querySelector("#submit");
+    submitButton.addEventListener("click", formSubmit, false);
+  });
+  function formSubmit(event) {
+    var toDoItem = document.getElementById("new-task-description").value;
+    if(toDoItem === undefined || toDoItem == null || toDoItem.length <= 0){
+      alert("To do item not provided");
+    }
+    else{
+      let ul = document.getElementById("list");
+      let li = document.createElement("li");
+      li.appendChild(document.createTextNode(toDoItem));
+      ul.appendChild(li);
+      document.getElementById("new-task-description").value = "";
+    }
+    event.preventDefault();
+  }
