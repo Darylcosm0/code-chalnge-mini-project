d98f4751359d2e16c3e96a5dafbe4f2c7266b96b
Darylcosm0 <mutaidaryl@gmail.com>
2022-12-05 10:39:47 +0300

    done

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
 //my js code
-
 document.addEventListener("DOMContentLoaded", () => {
-    const submitButton = document.querySelector("#submit");
-    submitButton.addEventListener("click", formSubmit, false);
-  });
-
-  function formSubmit(event) {
-    var toDoItem = document.getElementById("new-task-description").value;
-    if(toDoItem === undefined || toDoItem == null || toDoItem.length <= 0){
-      alert("To do item not provided");
-    }
-    else{
-      let ul = document.getElementById("list");
-
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(toDoItem));
-      ul.appendChild(li);
-      document.getElementById("new-task-description").value = "";
-    }
-    event.preventDefault();
-  }
+  getCharacters(characters)
+} )
+const url = "http://localhost:3000/characters"
+const characters = [
+  {
+   "id": 1,
+   "name": "Mr. Cute",
+   "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
+   "votes": 0
+  },
+  {
+      "id": 2,
+   "name": "Mx. Monkey",
+   "image": "https://t12humbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
+   "votes": 0
+  },
+  {
+      "id": 3,
+   "name": "Ms. Zebra",
+   "image": "https://media2.giphy.com/media/20G9uNqE3K4dRjCppA/source.gif",
+   "votes": 0
+  },
+  {
+      "id": 4,
+   "name": "Dr. Lion",
+   "image": "http://bestanimations.com/Animals/Mammals/Cats/Lions/animated-lion-gif-11.gif",
+   "votes": 0
+  },
+  {
+      "id": 5,
+   "name": "Mme. Panda",
+   "image": "https://media.giphy.com/media/ALalVMOVR8Qw/giphy.gif",
+   "votes": 0
+  },
+]
+function getCharacters(){
+  fetch (url)
+  .then (res => res.json())
+  .then(data => {
+      console.log(data)
+      data.forEach(characters => {
+          displayCharacters(characters)
+      });
+  })
+}
+function displayCharacters(characters){
+  console.log(characters.name);
+  const name = document.createElement('p');
+  name.innerHTML = characters.name
+  const main = document.getElementById("main")
+  main.append(name)
+  name.addEventListener("click",() => {
+      getSingleCharacters(characters)
+  })
+}
+function getSingleCharacters(characters){
+ document.getElementById("charactersname").innerHTML = characters.name
+ document.getElementById("charactersimage").src = characters.image
+ const btn=document.getElementById("characterslikes")
+ btn.textContent = `Likes ${characters.votes}`
+ btn.addEventListener("click", () =>{
+  characters.votes++;
+  btn.textContent= `Likes ${characters.votes}`
+})
+}
