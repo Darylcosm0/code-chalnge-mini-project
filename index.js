da8f86a8c52053c4d11150d88359f213e0e1cf21
Darylcosm0 <mutaidaryl@gmail.com>
2022-12-07 12:25:57 +0300

    done

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 //my js code
 document.addEventListener("DOMContentLoaded", () => {
-  getCharacters(characters)
+  getCharacters()
 } )
 const url = "http://localhost:3000/characters"
-const characters = [
-  {
-   "id": 1,
-   "name": "Mr. Cute",
-   "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
-   "votes": 0
-  },
-  {
-      "id": 2,
-   "name": "Mx. Monkey",
-   "image": "https://t12humbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
-   "votes": 0
-  },
-  {
-      "id": 3,
-   "name": "Ms. Zebra",
-   "image": "https://media2.giphy.com/media/20G9uNqE3K4dRjCppA/source.gif",
-   "votes": 0
-  },
-  {
-      "id": 4,
-   "name": "Dr. Lion",
-   "image": "http://bestanimations.com/Animals/Mammals/Cats/Lions/animated-lion-gif-11.gif",
-   "votes": 0
-  },
-  {
-      "id": 5,
-   "name": "Mme. Panda",
-   "image": "https://media.giphy.com/media/ALalVMOVR8Qw/giphy.gif",
-   "votes": 0
-  },
-]
+// const characters = [
+//   {
+//    "id": 1,
+//    "name": "Mr. Cute",
+//    "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
+//    "votes": 0
+//   },
+//   {
+//       "id": 2,
+//    "name": "Mx. Monkey",
+//    "image": "https://t12humbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
+//    "votes": 0
+//   },
+//   {
+//       "id": 3,
+//    "name": "Ms. Zebra",
+//    "image": "https://media2.giphy.com/media/20G9uNqE3K4dRjCppA/source.gif",
+//    "votes": 0
+//   },
+//   {
+//       "id": 4,
+//    "name": "Dr. Lion",
+//    "image": "http://bestanimations.com/Animals/Mammals/Cats/Lions/animated-lion-gif-11.gif",
+//    "votes": 0
+//   },
+//   {
+//       "id": 5,
+//    "name": "Mme. Panda",
+//    "image": "https://media.giphy.com/media/ALalVMOVR8Qw/giphy.gif",
+//    "votes": 0
+//   },
+// ]
 function getCharacters(){
   fetch (url)
   .then (res => res.json())
@@ -56,9 +56,11 @@ function displayCharacters(characters){
   })
 }
 function getSingleCharacters(characters){
- document.getElementById("charactersname").innerHTML = characters.name
- document.getElementById("charactersimage").src = characters.image
- const btn=document.getElementById("characterslikes")
+ document.getElementById("charname").innerHTML = characters.name
+ document.getElementById("charimage").src = characters.image
+ document.getElementById("charlikes").textContent= `Votes ${characters.votes}`
+
+ const btn=document.getElementById("charlikes")
  btn.textContent = `Likes ${characters.votes}`
  btn.addEventListener("click", () =>{
   characters.votes++;
